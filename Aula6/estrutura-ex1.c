6cb5750cdc0b8c16dc672d5bc0f0376737993cff
imarcaos <marcaos.melo@gmail.com>
2024-04-18 15:48:13 +0100

    Aula6- Struct - estrutura-ex1.c-final

--- Aula6/estrutura-ex1.c
+++ Aula6/estrutura-ex1.c
@@ -10,12 +10,15 @@
 Estrutura - Struct
 */
 
+
 int escolha;
 void menu();
 void inserir();
 void listagemGeral();
+void alterar();
+void listagemGenero();
 
-struct aluno {
+struct aluno{
     int numAluno;
     char nomeAluno[30];
     float notaFinal;
@@ -27,114 +30,156 @@ struct aluno al[20];
 
 int main(){
     setlocale(LC_ALL,"Portuguese");
-    system("cls");
-
+    
     menu();
-
+   
     return 0;
 }
 
-void menu() {
+void menu(){
     system("cls");
-    printf("Gestão de Alunos");
-    printf("\n1-Inserir Aluno");
-    printf("\n2-Alerar dados do aluno");
+    printf("Gestão de alunos");
+    printf("\n\n1-Inserir Aluno");
+    printf("\n2-Alterar dados do aluno");
     printf("\n3-Listagem dos alunos");
     printf("\n4-Listagem por género");
     printf("\n0-Sair");
-    printf("\nDigite a sua escolha: ");
-    scanf("%d", &escolha);
-
-    if(escolha==1) {
+    printf("\nDigite a sua escolha:");
+    scanf("%d",&escolha);
+    if(escolha==0){
+        printf("\nFim do programa\n");
+        Sleep(2000);
+        return;
+    }
+    else if(escolha==1){
         inserir();
         menu();
     }
-    else if (escolha==3) {
+   else if(escolha==2){
+        alterar();
+        menu();
+    }    
+    else if(escolha==3){
         listagemGeral();
         menu();
     }
+    else if(escolha==4){
+        listagemGenero();
+        menu();
+    } 
+   else{
+        printf("\nOpção inválida\n");
+        Sleep(2000);
+        menu();
+    }      
+  
 }
 
-void inserir() {
+void inserir(){
     system("cls");
     printf("Registar aluno\n");
-    if(al[19].numAluno!=0) {
+    int numero=0;
+    if(al[19].numAluno!=0){
         printf("Estrutura completa.\n");
         Sleep(2000);
         return;
     }
-    for(int i=0; i<20; i++) {
+    for(int i=0;i<20;i++){
         if(al[i].numAluno==0){
-            printf("Número:");
-            scanf("%d", &al[i].numAluno);
+
+            do{
+                printf("\nNúmero:");
+                scanf("%d",&numero);
+            }while(numero==0);
+
+            al[i].numAluno=numero;
             fflush(stdin);
-            printf("\nNome:");
+            printf("Nome:");
             gets(al[i].nomeAluno);
-            printf("\nNota final:");
-            scanf("%f", &al[i].notaFinal);
+            do{
+                printf("\nNota final:");
+                scanf("%f",&al[i].notaFinal);
+            }while(al[i].notaFinal<0 || al[i].notaFinal>20);
+
             fflush(stdin);
-            printf("\nGénero: (M)asculino / (F)eminino: ");
-            scanf("%c", &al[i].generoAluno);
+            printf("Género: (M)asculino / (F)eminino:");
+            scanf("%c",&al[i].generoAluno);
             break;
         }
     }
 }
-
-void listagemGeral() {
+void listagemGeral(){
     system("cls");
-    printf("Listagem de Aluno\n");
-    for(int i=0;i<20;i++) {
-        if(al[i].numAluno!=0) {
-            printf("\n%d", al[i].numAluno);
-            printf(" | %s", al[i].nomeAluno);
-            printf(" | %.2f", al[i].notaFinal);
-            printf(" | %c", al[i].generoAluno);
+    printf("Listagem de aluno\n");
+    for(int i=0;i<20;i++){
+        if(al[i].numAluno!=0){
+            printf("\n%d",al[i].numAluno);
+            printf(" | %s",al[i].nomeAluno);
+            printf(" | %.2f",al[i].notaFinal);
+            printf(" | %c",al[i].generoAluno);
         }
     }
     printf("\n\nFim da listagem. Aguarde um pouco sff.\n");
     Sleep(2000);
 }
-
-void alterar() {
+void alterar(){
     int numero;
     char confirma;
     system("cls");
-    printf("\nAlerar dados de alunos\n");
+    printf("\nAlterar dados de alunos\n");
     printf("Digite o número de aluno:");
-    scanf("%d", &numero);
-    for(int i=0; i<20; i++) {
-        if(al[i].numAluno==numero) {
-            printf("\n%d", al[i].numAluno);
-            printf(" | %s", al[i].nomeAluno);
-            printf(" | %.2f", al[i].notaFinal);
-            printf(" | %c", al[i].generoAluno);
+    scanf("%d",&numero);
+    for(int i=0;i<20;i++){
+        if(al[i].numAluno==numero){
+            printf("\n%d",al[i].numAluno);
+            printf(" | %s",al[i].nomeAluno);
+            printf(" | %.2f",al[i].notaFinal);
+            printf(" | %c",al[i].generoAluno);    
 
             fflush(stdin);
-            printf("\nDigite Ss para alterar o nome: ");
-            scanf("%c", &confirma);  
-            if(confirma=='S' || confirma=='s') {
-                printf("\nNome:");
-                gets(al[i].nomeAluno);             
-            }
-
+            printf("\nDigite Ss para alterar o nome:");
+            scanf("%c",&confirma);
             fflush(stdin);
-            printf("\nDigite Ss para alterar a nota: ");
-            scanf("%c", &confirma);  
-            if(confirma=='S' || confirma=='s') {
-                printf("\nNota final:");
-                scanf("%f", &al[i].notaFinal);                
+            if(confirma=='S' || confirma=='s'){
+                printf("Nome:");
+                gets(al[i].nomeAluno);
             }
-
             fflush(stdin);
-            printf("\nDigite Ss para alterar o género: ");
-            scanf("%c", &confirma);  
-            if(confirma=='S' || confirma=='s') {
-                printf("\nGénero: (M)asculino / (F)eminino: ");
-                scanf("%c", &al[i].generoAluno);                
+            printf("\nDigite Ss para alterar a nota:");
+            scanf("%c",&confirma);
+            if(confirma=='S' || confirma=='s'){
+                printf("Nota final:");
+                scanf("%f",&al[i].notaFinal);
             }
-            printf("\n\nFim da listagem. Aguarde um ouco sff.\n");
-            Sleep(2000);
+            fflush(stdin);
+            printf("\nDigite Ss para alterar o género:");
+            scanf("%c",&confirma);
+            fflush(stdin);
+            if(confirma=='S' || confirma=='s'){
+                printf("Género: (M)asculino / (F)eminino:");
+                scanf("%c",&al[i].generoAluno);
+            }
+            printf("\n\nFim da listagem. Aguarde um pouco sff.\n");
+            Sleep(2000);            
             break;
         }
     }
+}
+void listagemGenero(){
+    char genero;
+    system("cls");
+    printf("Listagem de alunos por género\n");
+    fflush(stdin);
+    printf("Digite o género: (M)asculino / (F)eminino:");
+    scanf("%c",&genero);
+    for(int i=0;i<20;i++){
+        if(al[i].numAluno!=0 && al[i].generoAluno==genero){
+            printf("\n%d",al[i].numAluno);
+            printf(" | %s",al[i].nomeAluno);
+            printf(" | %.2f",al[i].notaFinal);
+            printf(" | %c",al[i].generoAluno);
+        }
+    }
+    printf("\n\nFim da listagem. Aguarde um pouco sff.\n");
+    Sleep(2000);
 }
